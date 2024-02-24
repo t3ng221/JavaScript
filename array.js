210c41981e1569726bf79508a82bea9e11cc4c6d
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-02-24 19:53:39 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,39 @@
+// print an array
+const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+console.log(arr);
+// access array elements
+console.log(arr[5])
+//find length
+let arrLen = arr.length;
+console.log('Array Length: '+ arrLen);
+//find last element
+console.log('Last Element: '+ arr[arrLen-1]);
+//find first element
+console.log('First Element: '+ arr[0]);
+//find middle element
+if(arrLen%2 == 0){
+    let half = arrLen/2;
+    console.log('Middle Array: '+ arr[half]);
+}
+else{
+    console.log(arr[(Math.floor(arrLen/2))]);
+}
+//Modifying the element
+console.log('Previous Element: '+arr[2]);
+arr[2]= 99;
+console.log('Modified Element: '+ arr[2]);
+//Adding new elements Push method
+arr.push('Siam');
+console.log(arr);
+
+//Removing last element
+arr.pop();
+console.log(arr);
+
+//Removing 1st element
+arr.shift();
+console.log(arr);
+
+console.log('Previous Array: '+arr)
+arr.splice(2,4);
+console.log(arr);
---
+++ function.js
@@ -0,0 +1,11 @@
+function sum(a,b){
+ return a+b;
+ 
+}
+console.log(sum(5,7));
+
+function doubleIt(a){
+    return a*2;
+}
+console.log(doubleIt(50));
+
